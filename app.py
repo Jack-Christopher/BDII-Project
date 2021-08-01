ba258d169d719d24a598fad7b18ff0a31c59f784
Jack-Christopher <64046377+Jack-Christopher@users.noreply.github.com>
2021-07-31 20:18:42 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 import os 
 from cassandra.cluster import Cluster
-from flask import Flask, render_template, request, send_from_directory  
+from cassandra.query import dict_factory
+from flask import Flask, render_template, request, send_from_directory , redirect
 
+cluster = Cluster(contact_points=['127.0.0.1'], port=9042)
 
-def connect():
-	cluster = Cluster(contact_points=['127.0.0.1'], port=9042)
+def connect():	
 	session = cluster.connect()
 	return session
 
-def create_keyspace(session):
-	session.execute("""CREATE KEYSPACE bd WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '1'} 
-				AND durable_writes = 'true';""")
+def run_cql(session, fileName):
+	f = open(fileName, 'r')
+	txt = f.read()
+	statement_list = txt.split(';')
+	for statement in statement_list:
+		stmt = statement.strip()
+		if stmt != '':
+			#print('Executing "' + stmt + '"')
+			session.execute(stmt)
 
 
 
@@ -18,11 +25,39 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+	session = connect()
+	run_cql(session, "Creation.cql")
+	run_cql(session, "Insertions.cql")
 	return render_template("index.html")
 
 
+@app.route('/login_page.html', methods=['GET', 'POST'])
+def login():
+	error = None
+
+	if request.method == 'POST':
+		session = connect()
+		session.row_factory = dict_factory
+		query = """
+		 SELECT COUNT(*)
+		 FROM BD.Empleado 
+		 WHERE username = '""" + request.form['user_name'] + """' AND
+		 password = '""" + request.form['password'] + """' ALLOW FILTERING; """
+		
+		result = session.execute(query)[0]
+
+		if result['count'] < 1:
+			error = 'Datos incorrectos, inténtelo de nuevo'
+		else:
+			return redirect('main_view.html')
+	if request.method == 'GET':
+			error = None
+	return render_template('login_page.html', error=error)
+
+
+
 @app.route('/<page>')
-def HTML(page):
+def web_dir(page):
 	return render_template("/" + page)
 
 @app.route('/favicon.ico') 
