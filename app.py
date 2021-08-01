7e49bfbdfede0cbeebbba48ba2cab2cc2fa136d6
Jack-Christopher <64046377+Jack-Christopher@users.noreply.github.com>
2021-07-31 22:50:09 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -3,10 +3,15 @@ from cassandra.cluster import Cluster
 from cassandra.query import dict_factory
 from flask import Flask, render_template, request, send_from_directory , redirect
 
+# Global values :
 cluster = Cluster(contact_points=['127.0.0.1'], port=9042)
+employee_name = ""
+client_name = ""
+error = ""
 
 def connect():	
 	session = cluster.connect()
+	session.row_factory = dict_factory
 	return session
 
 def run_cql(session, fileName):
@@ -37,22 +42,78 @@ def login():
 
 	if request.method == 'POST':
 		session = connect()
-		session.row_factory = dict_factory
 		query = """
-		 SELECT COUNT(*)
-		 FROM BD.Empleado 
-		 WHERE username = '""" + request.form['user_name'] + """' AND
-		 password = '""" + request.form['password'] + """' ALLOW FILTERING; """
+			SELECT COUNT(*) FROM BD.Empleado 
+			WHERE username = '""" + request.form['user_name'] + """' AND
+			password = '""" + request.form['password'] + """' ALLOW FILTERING; """
 		
 		result = session.execute(query)[0]
 
 		if result['count'] < 1:
 			error = 'Datos incorrectos, inténtelo de nuevo'
 		else:
+			query2 = """ 
+				SELECT nombre FROM BD.Empleado
+				WHERE username = '""" + request.form['user_name'] + """'ALLOW FILTERING; """
+			result2 = session.execute(query2)[0]
+
+			global employee_name
+			employee_name = result2['nombre']
+			print(employee_name)
 			return redirect('main_view.html')
-	if request.method == 'GET':
-			error = None
+
+	#if request.method == 'GET':
+	#		error = None
 	return render_template('login_page.html', error=error)
+
+
+@app.route('/main_view.html')
+def view():
+	
+	global employee_name
+	print(employee_name)
+	if employee_name != "":
+		return render_template("main_view.html", employee_name = employee_name)
+	else:
+		error = "No ha iniciado sesión"
+		return redirect('login_page.html')
+
+
+
+@app.route('/start_sale.html', methods=['GET', 'POST'])
+def login_client():
+	error = None
+	if request.method == 'POST':
+		session = connect()
+		query = """
+			SELECT COUNT(*) FROM BD.Cliente 
+			WHERE DNI = """ + request.form['client_DNI'] + """  ALLOW FILTERING; """
+		
+		result = session.execute(query)[0]
+
+		if result['count'] < 1:
+			error = 'DNI incorrecto, inténtelo de nuevo'
+		else:
+			query2 = """ 
+				SELECT nombre FROM BD.Cliente
+				WHERE DNI = """ + request.form['client_DNI'] + """  ALLOW FILTERING; """
+			result2 = session.execute(query2)[0]
+
+			global client_name
+			client_name = result2['nombre']
+			print(client_name)
+			return redirect('main_client_view.html')
+
+	#if request.method == 'GET':
+	#		error = None
+	return render_template('start_sale.html', error=error)
+
+
+@app.route('/main_client_view.html', methods=['GET', 'POST'])
+def client_view():
+	return render_template("main_client_view.html", client_name = client_name)
+
+
 
 
 
