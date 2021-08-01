d204006fbafcf8d90f42816c94b4e33aff187d15
Jack-Christopher <64046377+Jack-Christopher@users.noreply.github.com>
2021-08-01 09:32:21 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 import os 
 from cassandra.cluster import Cluster
 from cassandra.query import dict_factory
-from flask import Flask, render_template, request, send_from_directory , redirect
+from flask import Flask, render_template, request, send_from_directory , redirect, session
 
 # Global values :
 cluster = Cluster(contact_points=['127.0.0.1'], port=9042)
-employee_name = ""
-client_name = ""
-error = ""
+
 
 def connect():	
-	session = cluster.connect()
-	session.row_factory = dict_factory
-	return session
+	conection = cluster.connect()
+	conection.row_factory = dict_factory
+	return conection
 
-def run_cql(session, fileName):
+def run_cql(conection, fileName):
 	f = open(fileName, 'r')
 	txt = f.read()
 	statement_list = txt.split(';')
@@ -22,32 +20,33 @@ def run_cql(session, fileName):
 		stmt = statement.strip()
 		if stmt != '':
 			#print('Executing "' + stmt + '"')
-			session.execute(stmt)
+			conection.execute(stmt)
 
 
 
 app = Flask(__name__)
+app.secret_key = 'very-secret-key'
 
 @app.route('/')
 def index():
-	session = connect()
-	run_cql(session, "Creation.cql")
-	run_cql(session, "Insertions.cql")
+	conection = connect()
+	run_cql(conection, "Creation.cql")
+	run_cql(conection, "Insertions.cql")
+	print("ok")
 	return render_template("index.html")
 
 
 @app.route('/login_page.html', methods=['GET', 'POST'])
 def login():
 	error = None
-
 	if request.method == 'POST':
-		session = connect()
+		conection = connect()
 		query = """
 			SELECT COUNT(*) FROM BD.Empleado 
 			WHERE username = '""" + request.form['user_name'] + """' AND
 			password = '""" + request.form['password'] + """' ALLOW FILTERING; """
 		
-		result = session.execute(query)[0]
+		result = conection.execute(query)[0]
 
 		if result['count'] < 1:
 			error = 'Datos incorrectos, inténtelo de nuevo'
@@ -55,25 +54,21 @@ def login():
 			query2 = """ 
 				SELECT nombre FROM BD.Empleado
 				WHERE username = '""" + request.form['user_name'] + """'ALLOW FILTERING; """
-			result2 = session.execute(query2)[0]
+			result2 = conection.execute(query2)[0]
 
-			global employee_name
-			employee_name = result2['nombre']
-			print(employee_name)
+			session['employee_name'] = result2['nombre']
 			return redirect('main_view.html')
 
-	#if request.method == 'GET':
-	#		error = None
+	elif request.method == 'GET':
+		if 'employee_name' in session:
+			return redirect('main_view.html')
 	return render_template('login_page.html', error=error)
 
 
 @app.route('/main_view.html')
 def view():
-	
-	global employee_name
-	print(employee_name)
-	if employee_name != "":
-		return render_template("main_view.html", employee_name = employee_name)
+	if session['employee_name'] != "":
+		return render_template("main_view.html", employee_name = session['employee_name'])
 	else:
 		error = "No ha iniciado sesión"
 		return redirect('login_page.html')
@@ -82,14 +77,13 @@ def view():
 
 @app.route('/start_sale.html', methods=['GET', 'POST'])
 def login_client():
-	error = None
 	if request.method == 'POST':
-		session = connect()
+		conection = connect()
 		query = """
 			SELECT COUNT(*) FROM BD.Cliente 
 			WHERE DNI = """ + request.form['client_DNI'] + """  ALLOW FILTERING; """
 		
-		result = session.execute(query)[0]
+		result = conection.execute(query)[0]
 
 		if result['count'] < 1:
 			error = 'DNI incorrecto, inténtelo de nuevo'
@@ -97,22 +91,37 @@ def login_client():
 			query2 = """ 
 				SELECT nombre FROM BD.Cliente
 				WHERE DNI = """ + request.form['client_DNI'] + """  ALLOW FILTERING; """
-			result2 = session.execute(query2)[0]
+			result2 = conection.execute(query2)[0]
 
-			global client_name
-			client_name = result2['nombre']
-			print(client_name)
+			session['client_name'] = result2['nombre']
 			return redirect('main_client_view.html')
 
-	#if request.method == 'GET':
-	#		error = None
+	if request.method == 'GET':
+		error = None
 	return render_template('start_sale.html', error=error)
 
 
-@app.route('/main_client_view.html', methods=['GET', 'POST'])
+@app.route('/main_client_view.html')
 def client_view():
-	return render_template("main_client_view.html", client_name = client_name)
+	if 'client_name' not in session:
+		#error = "No ha seleccionado a un cliente"
+		return redirect("start_sale.html")
+	else:
+		return render_template("main_client_view.html", 
+			client_name = session['client_name'])
 
+
+
+@app.route('/add_product.html')
+def add_product():
+	if 'client_name' not in session:
+		return redirect("start_sale.html")
+	else:
+		conection = connect()
+		query = "SELECT * FROM BD.Producto ALLOW FILTERING; "
+		result = conection.execute(query)
+
+		return render_template('add_product.html', products = result)
 
 
 
